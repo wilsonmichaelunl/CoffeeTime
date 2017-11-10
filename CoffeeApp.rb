0011da52fed133c53d997c479338675214c70674
Michael Wilson <michael.wilson@firespring.com>
2017-11-10 17:45:31 -0600

    added to ruby

--- CoffeeApp.rb
+++ CoffeeApp.rb
@@ -21,3 +21,23 @@ difference_in_hours = difference_in_minutes / 60
 leftover_minutes = difference_in_minutes % 60 
 
 puts "The time difference is #{difference_in_hours} hours and #{leftover_minutes} minutes" 
+
+minutes_ahead = Integer(difference_in_minutes * 1.2)
+difference_in_hours = minutes_ahead / 60
+leftover_minutes = minutes_ahead % 60
+
+puts "The time on the coffee maker will elapse: #{difference_in_hours} hours and #{leftover_minutes} minutes"
+puts "What time is it on your coffee maker? Please enter in 24 hour HH:MM format"
+
+time_on_maker = gets.chomp
+machine_in_minutes = (time_on_maker.split(':')[0].strip.to_i * 60) + time_on_maker.split(':')[1].strip.to_i
+total_minutes = machine_in_minutes + minutes_ahead
+
+new_hours = total_minutes / 60
+new_minutes = total_minutes % 60
+
+puts "Set your coffee maker to make coffee at: #{new_hours}:#{new_minutes}"
+
+
+
+
